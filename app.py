8afa841488537c2f1d971bdd492d25d7448322ef
199921 <199921.jung@gmail.com>
2021-12-12 18:08:58 +0900

    39주문서 구조 작업완료

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	return render_template('index_user.html')
 
-@app.route('/mypage')
-def mypage():
-	return render_template('mypage.html')
+@app.route('/index_admin')
+def index_admin():
+	return render_template('index_admin.html')
 
-@app.route('/admin')
-def admin():
-	return render_template('admin.html')
+@app.route('/index_admin_lists')
+def index_admin_lists():
+	return render_template('index_admin_lists.html')
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=8080, debug=True)
