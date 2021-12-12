d67ccae0b8339fdbe260d0f713904a56011089ed
199921 <199921.jung@gmail.com>
2021-12-12 17:38:19 +0900

    세번째 커밋 - 구조화 작업 완료

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+	return render_template('index.html')
 
+@app.route('/mypage')
+def mypage():
+	return render_template('mypage.html')
+
+@app.route('/admin')
+def admin():
+	return render_template('admin.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0',port=5000,debug=True)
