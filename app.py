371587fb4d1d4a71d957a46e10ad51250dd31437
199921 <199921.jung@gmail.com>
2021-12-15 01:23:17 +0900

    bg-1 까지 완료

--- app.py
+++ app.py
@@ -5,15 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('admin.html')
-
-@app.route('/index')
-def index_admin():
-	return render_template('index.html')
-
-@app.route('/mypage')
-def index_admin_lists():
-	return render_template('mypage.html')
+	return render_template('index_UPNOON.html')
 
 if __name__ == '__main__':
     app.run()
